eb355501f4fb6e727f474fef55e5e120bb5e588b
Beaugrand, Kevin <kevin.beaugrand@cgi.com>
2021-10-10 17:26:55 +0200

    Add coverage in CI

--- src/modules/WattecoDecoderModule/replaceCodecImports.sh
+++ src/modules/WattecoDecoderModule/replaceCodecImports.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-sed -i 's/from ZCL/from .ZCL/'                          ./Standard/srcWatteco/Decoding_Functions.py
-sed -i 's/from ZCL/from .ZCL/'                          ./Standard/srcWatteco/ZCL_FRAME.py
-sed -i 's/from ZCL/from .ZCL/'                          ./Standard/srcWatteco/ZCL.py
-sed -i 's/from WTC_CodecTools/from .WTC_CodecTools/'    ./Standard/srcWatteco/ZCL.py
-sed -i 's/from TICs/from .TICs/'                        ./Standard/srcWatteco/ZCL.py
-sed -i 's/from WTC_CodecTools/from ..WTC_CodecTools/'   ./Standard/srcWatteco/TICs/_TIC_Tools.py
+sed -i 's/from ZCL/from .ZCL/'                          ./Standard/Decoding_Functions.py
+sed -i 's/from ZCL/from .ZCL/'                          ./Standard/ZCL_FRAME.py
+sed -i 's/from ZCL/from .ZCL/'                          ./Standard/ZCL.py
+sed -i 's/from WTC_CodecTools/from .WTC_CodecTools/'    ./Standard/ZCL.py
+sed -i 's/from TICs/from .TICs/'                        ./Standard/ZCL.py
+sed -i 's/from WTC_CodecTools/from ..WTC_CodecTools/'   ./Standard/TICs/_TIC_Tools.py
 sed -i 's/import constants/from . import constants/'    ./Batch/br_uncompress.py
